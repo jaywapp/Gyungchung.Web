d3427a1b6dea6202b74a07c91390603578e99d5c
unknown <jaywapp16@gmail.com>
2023-12-30 17:10:04 +0900

    initialize

---
+++ gyungchung/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './login'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  max-width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
+  return (    
+    <Div>
+      <Login/>
+    </Div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
